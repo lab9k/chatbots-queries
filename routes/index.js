8b668c67305b31c66afb666b0e438e0fbbc5c9c9
Jefwillems <jefwillems100@gmail.com>
2019-04-15 18:17:57 +0200

    add airtable feedback logging

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 var nalantisApi = require('../services/nalantisApi');
 var alexandriaApi = require('../services/alexandriaApi');
+const airtableApi = require('../services/airtable.api');
 
 router.post('/responses', function(req, res, next) {
   const searches = [
@@ -13,6 +14,19 @@ router.post('/responses', function(req, res, next) {
       res.json({ nalantis: results[0], alexandria: results[1] });
     })
     .catch(err => console.log(err));
+});
+
+router.post('/vote', (req, res) => {
+  const { positive, item, question } = req.body;
+  const api = new airtableApi();
+  api.addLine({
+    question,
+    feedback: positive,
+    document: item.uuid || item.resourceURI,
+    provider: item.from,
+    sessionid: 'search-session'
+  });
+  return res.status(200).json({ message: 'success' });
 });
 
 module.exports = router;
---
+++ services/airtable.api.js
@@ -0,0 +1,60 @@
+const Airtable = require('airtable');
+
+module.exports = class AirtableApi {
+  constructor() {
+    this.API_KEY = process.env.AIRTABLE_API_KEY;
+    if (!this.API_KEY) {
+      throw 'No Airtable api key specified';
+    }
+    this.nalantis_base = Airtable.base('app26Szli30UQb1Xh');
+    this.alexandria_base = Airtable.base('app4n7fT9HFiM0gwj');
+  }
+
+  /**
+   *
+   *
+   * @param {{question:string,feedback:boolean,document:string,sessionid:string,provider:string}} options
+   * @returns
+   * @memberof AirtableApi
+   */
+  addLine(options) {
+    if (options.provider === 'nalantis') {
+      return this.nalantis_base('Sessions').create(
+        {
+          sessionid: options.sessionid,
+          question: options.question,
+          Date: new Date(Date.now()).toISOString(),
+          'document returned': options.document,
+          Feedback: options.feedback ? 'Good' : 'Bad'
+        },
+        (err, record) => {
+          if (err) {
+            console.log('error creating airtable record');
+            console.error(err);
+            return;
+          }
+          console.log('airtable record successfully created');
+          console.log(`airtable record id: ${record.getId()}`);
+        }
+      );
+    }
+    return this.alexandria_base('Sessions').create(
+      {
+        sessionid: options.sessionid,
+        question: options.question,
+        Date: new Date(Date.now()).toISOString(),
+        'document returned': options.document,
+        Feedback: options.feedback ? 'Good' : 'Bad'
+      },
+      (err, record) => {
+        if (err) {
+          console.log('error creating airtable record');
+          console.error(err);
+          return;
+        }
+        console.log('airtable record successfully created');
+        console.log(`airtable record id: ${record.getId()}`);
+      }
+    );
+  }
+};
